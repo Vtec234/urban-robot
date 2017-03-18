6431fa158cb45b51ce6d36da9c58a36c1751905d
Tadeusz Janik <tadeusz.janik.97@gmail.com>
2017-03-18 22:16:02 +0000

    filter and even more logic and some graphics

--- game.py
+++ game.py
@@ -11,51 +11,65 @@ import pygame
 # 5 - audienceGoal
 class Game:
     def __init__(self):
-        self._WIDTH = 256
-        self._HEIGHT = 256
+        self._WIDTH = 175
+        self._HEIGHT = 175
         self._PLAYERX = self._WIDTH/2
         self._PLAYERY = self._HEIGHT/2
+        self._MOVE = ""
         self._MONSTERS = []
+        self._HP = []
 
     # Runs every game tick (e.g. 1 second)
     def tick(self):
-        #for i in range(:
-            #self._MONSTERS.
-
-        if move == "up" and self._PLAYERY>0
+        if self._MOVE == "up" and self._PLAYERY>0:
             self._PLAYERY = self._PLAYERY - 1
-        elif move == "down" and self._PLAYERY<self._HEIGHT
+        elif self._MOVE == "down" and self._PLAYERY<self._HEIGHT:
             self._PLAYERY = self._PLAYERY + 1
-        elif move == "left" and self._PLAYERX>0
+        elif self._MOVE == "left" and self._PLAYERX>0:
             self._PLAYERX = self._PLAYERX - 1
-        elif move == "right" and self._PLAYERX<self._WIDTH
+        elif self._MOVE == "right" and self._PLAYERX<self._WIDTH:
             self._PLAYERX = self._PLAYERX - 1
+
+    def filter(list, maxPair, minPair):
+        newlist = []
+        for elem in list:
+            if (elem[0] >= minPair[0]) and (elem[0] <= maxPair[0]):
+                if (elem[1] >= minPair[1]) and (elem[1] <= maxPair[1]):
+                    pass
+
 
     def draw(self):
-        pass
-        # render graphics to screen
+        size = width, height = 400, 400
+        white = (255, 255, 255)
+        screen = pygame.display.set_mode(size)
+        pygame.display.update()
+        for i in range(0,10):
+            for j in range(0, 10):
+                pygame.draw.rect(screen, white, [i*40+2, j*40+2, 36, 36])
+                pygame.display.update()
+        #render graphics to screen
 
     # Runs every actual frame (e.g. 1MIL times/sec)
-    def update(self);
+    def update(self):
         for event in pygame.event.get() :
             if event.type == pygame.KEYDOWN :
                 if event.key == pygame.K_UP :
-                    move = "up"
+                    self._MOVE = "up"
                 elif event.key == pygame.K_DOWN :
-                    move = "down"
+                    self._MOVE = "down"
                 elif event.key == pygame.K_LEFT :
-                    move = "left"
+                    self._MOVE = "left"
                 elif event.key == pygame.K_RIGHT :
-                    move = "right"
-
+                    self._MOVE = "right"
         # This is true every second
         if (time_milliseconds % 1000) == 0:
             self.tick()
-
-        self.draw()
+            self.draw()
 
 def main():
     pygame.init()
     game = Game()
-    if True:
-        game.update()
+    game.draw()
+    while True:
+        #game.update()
+        pygame.event.get()
