4a03f257f34027f0698a18418cff2d3c43571708
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-03-18 22:31:44 +0000

    server

--- server.py
+++ server.py
@@ -3,17 +3,24 @@ from flask import request
 from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/world")
+@app.route("/world", methods = ["GET"])
 def handleGetWorld():
-    return "world"
+    return jsonify( { "player": (2, 4), "monsters": [(2, 4), (3, 7), (1, 0)], "healths": [()] } )
+
+@app.route("/goals", methods = ["GET"])
+def handleGetGoals():
+    return jsonify( { "playerGoal": (2, 5), "audienceGoal": (45, 3) } )
 
 @app.route("/spawnMonster", methods = ["POST"])
 def handleSpawnMonster():
     if request.method == "POST":
+        #return jsonify(game.get_monsters())
         # read coords, spawn
-        return "monster spawned"
+        print(request.form)
+        return "OK"
 
 @app.route("/spawnHealth", methods = ["POST"])
 def handleSpawnHealth():
     if request.method == "POST":
-        return "health spawned
+        print(request.form)
+        return "OK"
