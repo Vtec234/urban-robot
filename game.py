713019306b68f5fe46dd0f3b90324d41e2ab7303
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-03-18 19:43:15 +0000

    logic

--- game.py
+++ game.py
@@ -17,13 +17,26 @@ class Game:
         self._PLAYERY = self._HEIGHT/2
         self._MONSTERS = []
 
-        # Player starts at middle
-        self._map[WIDTH/2][HEIGHT/2] = 1
+    # Runs every game tick (e.g. 1 second)
+    def tick(self):
+        #for i in range(:
+            #self._MONSTERS.
 
-    def update(self):
+        if move == "up" and self._PLAYERY>0
+            self._PLAYERY = self._PLAYERY - 1
+        elif move == "down" and self._PLAYERY<self._HEIGHT
+            self._PLAYERY = self._PLAYERY + 1
+        elif move == "left" and self._PLAYERX>0
+            self._PLAYERX = self._PLAYERX - 1
+        elif move == "right" and self._PLAYERX<self._WIDTH
+            self._PLAYERX = self._PLAYERX - 1
+
+    def draw(self):
         pass
-        for i in range(:
-            self._MONSTERS.
+        # render graphics to screen
+
+    # Runs every actual frame (e.g. 1MIL times/sec)
+    def update(self);
         for event in pygame.event.get() :
             if event.type == pygame.KEYDOWN :
                 if event.key == pygame.K_UP :
@@ -34,29 +47,15 @@ class Game:
                     move = "left"
                 elif event.key == pygame.K_RIGHT :
                     move = "right"
-        if move == "up" && self._PLAYERY>0
-            self._PLAYERY = self._PLAYERY - 1
-        elif move == "down" && self._PLAYERY<self._HEIGHT
-            self._PLAYERY = self._PLAYERY + 1
-        elif move == "left" && self._PLAYERX>0
-            self._PLAYERX = self._PLAYERX - 1
-        elif move == "right" && self._PLAYERX<self._WIDTH
-            self._PLAYERX = self._PLAYERX - 1
 
+        # This is true every second
+        if (time_milliseconds % 1000) == 0:
+            self.tick()
 
-        # drawing
-
-    def draw(self):
-
-
-
-
+        self.draw()
 
 def main():
     pygame.init()
-    game = Gamea()
+    game = Game()
     if True:
         game.update()
-        game.draw()
-
-
