f834e960dc4ef5dd9d44855ce67e7233aa0c0119
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-03-19 00:26:04 +0000

    works now

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import math
 import random
+import time
 
 
 # World grid type:
@@ -42,141 +43,126 @@ class Game:
     def get_audience_goal(self):
         return self._agoal
 
-#firstly goals, then hps, then with time being audience introduce hps and monsters
-
-        #new monster, given coordinates  
-        #cannot be monster in the same place what another monster
-        #cannot be too close  
-    def new_mon(self, x, y):
-        #if too close to player
-        if distance(x,y,PLAYERX,PLAYERY) < _mindist
-            return False
-
-        #if too close to goals
-        if distance(x,y,_PGX,_PGY) < _mindist/2
-            return False
-
-        if distance(x,y,_AGX,_AGY) < _mindist/2
-            return False
-
-        #if too close to HPs    
-        for h in _HP :
-            h1 = _HP[h[0]]
-            h2 = _HP[h[1]]
-            a = distance(h1,h2,x,y)
-            if distance(a,b,x,y) < _mindist/2
-                return False
-
-        #if too close to monsters
-        for i in _MONSTERS:
-            a = _MONSTERS[i[0]]
-            b = _MONSTERS[i[1]]
-            a = distance(a,b,x,y)
-            if distance(a,b,x,y) < _mindist
-                return False
-        
-        _MONSTERS.append((x,y))
-        return True
-
-    #new health point
-    def new_hp (self, x, y)
-        #if too close to player
-        if distance(x,y,PLAYERX,PLAYERY) < _mindist
-            return False
-
-        #if too close to goals
-        if distance(x,y,_PGX,_PGY) < _mindist/2
-            return False
-
-        if distance(x,y,_AGX,_AGY) < _mindist/2
-            return False
-
-        #if too close to HPs    
-        for h in _HP :
-            h1 = _HP[h[0]]
-            h2 = _HP[h[1]]
-            a = distance(h1,h2,x,y)
-            if distance(a,b,x,y) < _mindist/2
-                return False
-
-        #if too close to monsters
-        for i in _MONSTERS:
-            a = _MONSTERS[i[0]]
-            b = _MONSTERS[i[1]]
-            a = distance(a,b,x,y)
-            if distance(a,b,x,y) < _mindist
-                return False
-
-        _HP.append((x,y))
-        return True
-
-    #distance between two points
+    @staticmethod
     def distance(a, b):
         dist = math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
         return dist
 
-    def add_goals(self):
-        # TODO random
-        self._pgoal = (20, 20)
-        while distance(self._pgoal, self._playerPos) > (10 * self._MINDIST):
-            self._pgoal = (20, 20)
-
-        self._agoal = (20, 20)
-        while distance(self._agoal, self._playerPos) > (10 * self._MINDIST) or distance(self._agoal, self._pgoal) > (5 * self._MINDIST):
-            self._agoal = (20, 20)
-
-    # Runs every game tick (e.g. 1 second)
-    def tick(self):
-        if self._MOVE == "up" and self._PLAYERY>0:
-            self._PLAYERY = self._PLAYERY - 1
-        elif self._MOVE == "down" and self._PLAYERY<self._HEIGHT:
-            self._PLAYERY = self._PLAYERY + 1
-        elif self._MOVE == "left" and self._PLAYERX>0:
-            self._PLAYERX = self._PLAYERX - 1
-        elif self._MOVE == "right" and self._PLAYERX<self._WIDTH:
-            self._PLAYERX = self._PLAYERX - 1
-
-    def filter(list, maxPair, minPair):
+    # Returns elements of list that fit between minPair and maxPair on both X-axis and Y-axis
+    @staticmethod
+    def filter(lst, maxPair, minPair):
         newlist = []
-        for elem in list:
+        for elem in lst:
             if (elem[0] >= minPair[0]) and (elem[0] <= maxPair[0]):
                 if (elem[1] >= minPair[1]) and (elem[1] <= maxPair[1]):
                     newlist.append(elem)
         return newlist
 
+    def not_too_close(self, pos):
+        if self.distance(pos, self._playerPos) < self._mindist:
+            return False
+
+        #if too close to goals
+        if self.distance(pos, self._pgoal) < self._mindist/2:
+            return False
+
+        if self.distance(pos, self._agoal) < self._mindist/2:
+            return False
+
+        #if too close to HPs
+        for ptn in self._potions:
+            if self.distance(ptn, pos) < self._mindist/2:
+                return False
+
+        #if too close to monsters
+        for mon in self._monsters:
+            if self.distance(mon, pos) < self._mindist:
+                return False
+
+        return True
+
+        #firstly goals, then hps, then with time being audience introduce hps and monsters
+        #new monster, given coordinates
+        #cannot be monster in the same place what another monster
+        #cannot be too close
+    def spawn_monster(self, pos):
+        #if too close to player
+        if self.not_too_close(pos):
+            self._monsters.append(pos)
+            return True
+
+        return False
+
+    # new potion
+    def spawn_potion(self, pos):
+        #if too close to player
+        if self.not_too_close(pos):
+            self._potions.append(pos)
+            return True
+
+        return False
+
+    def add_goals(self):
+        # TODO random
+        self._pgoal = (20, 20)
+        while self.distance(self._pgoal, self._playerPos) > (10 * self._mindist):
+            self._pgoal = (20, 20)
+
+        self._agoal = (20, 20)
+        while self.distance(self._agoal, self._playerPos) > (10 * self._mindist) or self.distance(self._agoal, self._pgoal) > (5 * self._mindist):
+            self._agoal = (20, 20)
+
+    # Runs every game tick (e.g. 1 second)
+    def tick(self):
+        if self._move == "up" and self._playerPos[1] > 0:
+            self._playerPos[1] -= 1
+        elif self._move == "down" and self._playerPos[1] < self._HEIGHT:
+            self._playerPos[1] += 1
+        elif self._move == "left" and self._playerPos[0] > 0:
+            self._playerPos[0] -= 1
+        elif self._move == "right" and self._playerPos[0] < self._WIDTH:
+            self._playerPos[0] += 1
 
     def draw(self):
         size = width, height = 700, 700
         white = (255, 255, 255)
         screen = pygame.display.set_mode(size)
-        pygame.display.update()
         for i in range(0, self._GRIDSIZE):
             for j in range(0, self._GRIDSIZE):
                 pygame.draw.rect(screen, white, [i*20+1, j*20+1, 18, 18])
+
         pygame.display.update()
-        #render graphics to screen
 
     # Runs every actual frame (e.g. 1MIL times/sec)
+    # Returns whether the update should run again (True) or game shoud be closed (False)
     def update(self):
-        for event in pygame.event.get() :
-            if event.type == pygame.KEYDOWN :
-                if event.key == pygame.K_UP :
-                    self._MOVE = "up"
-                elif event.key == pygame.K_DOWN :
-                    self._MOVE = "down"
-                elif event.key == pygame.K_LEFT :
-                    self._MOVE = "left"
-                elif event.key == pygame.K_RIGHT :
-                    self._MOVE = "right"
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self._move = "up"
+                elif event.key == pygame.K_DOWN:
+                    self._move = "down"
+                elif event.key == pygame.K_LEFT:
+                    self._move = "left"
+                elif event.key == pygame.K_RIGHT:
+                    self._move = "right"
+
+            elif event.type == pygame.QUIT:
+                return False
+
         # This is true every second
-        if (time_milliseconds % 1000) == 0:
+        if (int(time.time() * 1000.0)) % 1000 == 0:
             self.tick()
             self.draw()
+
+        return True
+
 
 def main():
     pygame.init()
     game = Game()
     game.draw()
-    while True:
-        #game.update()
-        pygame.event.get()
+    while game.update():
+        pass
+
+    pygame.quit()
--- server.py
+++ server.py
@@ -2,30 +2,32 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
-from game import Game
+#from game import Game
 
-game = Game()
+#game = Game()
 
 app = Flask(__name__)
 
 @app.route("/world", methods = ["GET"])
 def handleGetWorld():
-    return jsonify( { "player": game.get_player_pos(), "monsters": game.get_monsters(), "potions": game.get_potions() } )
+    #return jsonify( { "player": game.get_player_pos(), "monsters": game.get_monsters(), "potions": game.get_potions() } )
+    return jsonify( { "pos": (2, 4) } )
 
 @app.route("/goals", methods = ["GET"])
 def handleGetGoals():
-    return jsonify( { "playerGoal": game., "audienceGoal": (45, 3) } )
+    #return jsonify( { "playerGoal": game.get_player_goal(), "audienceGoal": game.get_audience_goal() } )
+    return "OK"
 
 @app.route("/spawnMonster", methods = ["POST"])
 def handleSpawnMonster():
     if request.method == "POST":
-        #return jsonify(game.get_monsters())
         # read coords, spawn
-        print(request.form)
-        return "OK"
+        data = request.get_json()
+        return jsonify(game.new_mon(data["pos"]))
 
 @app.route("/spawnHealth", methods = ["POST"])
 def handleSpawnHeal():
     if request.method == "POST":
-        print(request.form)
+        data = request.get_json()
+        print(data["pos"])
         return "OK"
